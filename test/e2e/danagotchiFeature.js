2c031cc7c20b93e8e010688595f93a8a50d8e22a
Marion LV <marion.levisage@gmail.com>
2016-03-16 11:49:57 +0000

    updated npm start to use Sinatra server, moved node_modules out of public folder and bower_components into public folder

--- test/e2e/danagotchiFeature.js
+++ test/e2e/danagotchiFeature.js
@@ -1,6 +1,6 @@
 describe('Danagotchi', function(){
   it("displays the page title and 'Hello World'", function(){
-    browser.get('http://localhost:8080');
+    browser.get('http://localhost:4567');
     element(by.id('username')).sendKeys("Ina");
     element(by.buttonText('Submit')).click();
     expect(browser.getTitle()).toEqual('DANAGOTCHI');
