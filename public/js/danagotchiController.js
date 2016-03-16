384315e656fedb5db4d3f90da304da39fbbe0c01
JoeWroe <joe_wroe@icloud.com>
2016-03-16 17:50:24 +0000

    Asks name and Greets

--- public/js/danagotchiController.js
+++ public/js/danagotchiController.js
@@ -1,8 +1,18 @@
 danagotchi.controller('DanagotchiController', ['$resource', function($resource){
 	var self = this;
 
-	self.displayName;
-	self.addDisplayName = function(newDisplayName) {
-		self.displayName = newDisplayName;
+	self.nameQuery;
+
+	self.submitNameQuery = function(newNameQuerySubmit) {
+		self.nameQuery = newNameQuerySubmit;
 	};
+
+	self.isNameQuerySet = function() {
+		return !!self.nameQuery;
+	};
+
+	// self.hello = function() {
+	// 	return true;
+	// };
+
 }]);
--- test/e2e/danagotchiFeature.js
+++ test/e2e/danagotchiFeature.js
@@ -1,9 +1,17 @@
 describe('Danagotchi', function(){
-  it("displays the page title and 'Hello World'", function(){
+  it("asks for your name and greets you.", function(){
     browser.get('http://localhost:4567');
-    element(by.id('username')).sendKeys("Ina");
-    element(by.buttonText('Submit')).click();
-    expect(browser.getTitle()).toEqual('DANAGOTCHI');
-    expect(element(by.id('greeting')).getText()).toBe('Hello Ina!');
+    expect(element(by.id('nameQuery')).isDisplayed()).toBe(true);
+    expect(element(by.id('nameToDisplay')).isDisplayed()).toBe(true);
+    expect(element(by.id('greeting')).isDisplayed()).toBe(false);
+    element(by.id('nameToDisplay')).sendKeys("Joe");
+    element(by.id('nameQuery')).submit();
+    expect(element(by.id('nameQuery')).isDisplayed()).toBe(false);
+    expect(element(by.id('nameToDisplay')).isDisplayed()).toBe(false);
+    expect(element(by.id('greeting')).isDisplayed()).toBe(true);
+    expect(element(by.id('greeting')).getText()).toBe('Hello, Joe!');
+    // expect(element(by.id('greeting')).getText()).toBe('Hello Ina!');
   });
+
+
 });
