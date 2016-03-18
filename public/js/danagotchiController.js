6de9331c2e3f5e1cc4fd8d9101bf2efd12bf7eae
JoeWroe <joe_wroe@icloud.com>
2016-03-18 10:50:56 +0000

    Emotion button messing

--- public/js/danagotchiController.js
+++ public/js/danagotchiController.js
@@ -7,7 +7,7 @@ danagotchi.controller('DanagotchiController', ['$resource', 'Quote', function($r
 	self.addDisplayName = function(newDisplayName) {
 		localStorage.setItem('displayName', newDisplayName);
 		self.displayName = localStorage.getItem('displayName');
-	}
+	};
 
 	self.isDisplayNameDefined = function() {
 		return !!self.displayName;
@@ -17,21 +17,24 @@ danagotchi.controller('DanagotchiController', ['$resource', 'Quote', function($r
 		localStorage.setItem('gratitudeInput', newGratitudeInput);
 		self.gratitudeInput  = localStorage.getItem('gratitudeInput');
 	};
+
 	self.isGratitudeInputDefined = function() {
 		return !!self.gratitudeInput;
 	};
+
 	self.showButton = function() {
 		self.show = true;
-	}
+	};
+
 	self.hideButton = function() {
 		timeOut(function(){
 			self.show = false;
 		}, 1000);
-	}
+	};
 
 	self.delete = function(){
 		self.gratitudeInput = localStorage.removeItem('gratitudeInput');
-	}
+	};
 
 	Quote.getQuote().then(function(quoteData){
 		self.quote = quoteData;
